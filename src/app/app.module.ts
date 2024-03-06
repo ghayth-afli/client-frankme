40f0355976e5e2c2664e17fabfe2d6440f1fdcea
Afli Mohamed Ghayth <40703798+ghayth-afli@users.noreply.github.com>
2024-03-06 22:36:45 +0100

    create client side with auth pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './core/authentication/components/register/register.component';
+import { ForgetPasswordComponent } from './core/authentication/components/forget-password/forget-password.component';
+import { LoginComponent } from './core/authentication/components/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgetPasswordComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
