abba4877fc238a51caf788dde300cb2b87f8fcbe
Afli Mohamed Ghayth <40703798+ghayth-afli@users.noreply.github.com>
2024-03-06 23:17:16 +0100

    add forgot password page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './core/authentication/components/login/login.component';
 import { RegisterComponent } from './core/authentication/components/register/register.component';
+import { ForgotPasswordComponent } from './core/authentication/components/forgot-password/forgot-password.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './core/authentication/components/register/register.component';
-import { ForgetPasswordComponent } from './core/authentication/components/forget-password/forget-password.component';
 import { LoginComponent } from './core/authentication/components/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ForgotPasswordComponent } from './core/authentication/components/forgot-password/forgot-password.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    ForgetPasswordComponent,
+    ForgotPasswordComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
